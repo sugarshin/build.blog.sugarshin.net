e52e31881573c51f6d396d3096c0c83125326f20
Shingo Sato <shinsugar@gmail.com>
2022-09-26 22:09:02 +0900

    tweak

--- monthly-report.sh
+++ monthly-report.sh
@@ -13,12 +13,22 @@ NAME=${CIRCLE_USERNAME:-'CircleCI'}
 cd "$DIR"
 git checkout -b "$BRANCH" || git checkout "$BRANCH"
 git pull --depth=1 origin "$BRANCH" || true
+
 nvm install
+npm install --global yarn
+echo "Node.js: $(node -v)"
+echo "npm:     $(npm -v)"
+echo "Yarn:    $(yarn --version)"
+
 yarn
 npm run mr -- -p 31 -u minutes -n "$NAME" -U "$CIRCLE_BUILD_URL"
+
+git config --global user.name 'CircleCI'
+git config --global user.email 's+circleci@sugarshin.net'
 git add --all
 git commit -m "$TITLE"
 git push origin HEAD:"$BRANCH"
 cd -
+
 npm run create-pull-request -- -b "$BRANCH" -t "$TITLE"
 npm run merge-pull-request -- -b "$BRANCH"
